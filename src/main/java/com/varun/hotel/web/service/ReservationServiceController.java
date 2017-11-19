5d8bf81b86f34eaacf2a6131b9869ef937616f56
Varun Upadhyay <varun.u28@gmail.com>
2017-11-18 19:07:03 -0800

    Exposed a service layer through REST

---
+++ src/main/java/com/varun/hotel/web/service/ReservationServiceController.java
@@ -0,0 +1,25 @@
+package com.varun.hotel.web.service;
+
+import com.varun.hotel.business.domain.RoomReservation;
+import com.varun.hotel.business.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api")
+public class ReservationServiceController {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/reservations/{date}")
+    public List<RoomReservation> getAllRoomReservationsForDate(@PathVariable(value = "date")String dateString) {
+
+        return this.reservationService.getRoomReservationsForDate(dateString);
+    }
+}
